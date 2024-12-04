eb6ab6dd6141f37a0912b5783f07a30632b581d7
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-12-04 04:33:47 +0100

    codebase refactor

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+#### Stop Watch
 
+#### Languages used
+
+Html
+css
+javascript
+
+using javascript functionality and a it design and styling from css , I decided to create a normal and random StopWatch............
+Want to check it out click on the link below
+
+#### StopWatch :
--- script.js
+++ script.js
@@ -1,64 +1,65 @@
-let millisecons = document.querySelector(".miliseconds");
 let seconds = document.querySelector(".seconds");
 let minutes = document.querySelector(".minutes");
+let hours = document.querySelector(".hours");
 let btn = document.querySelectorAll(".btn");
-let startmilli = 0;
 let sec = 0;
-let min = 0;
+let mins = 0;
+let hrs = 0;
 let timeinterval;
 btn.forEach((Each) => {
   Each.addEventListener("click", (e) => {
-    let clickedbtn = e.currentTarget.classList;
-    //  start button
-    if (clickedbtn.contains("start")) {
-      timeinterval = setInterval(onstart, 16.67);
+    // values from dom
+    const upDateText = (sec, mins, hr) => {
+      seconds.textContent = twoDigit(sec);
+      minutes.textContent = twoDigit(mins);
+      hours.textContent = twoDigit(hr);
+    };
+
+    // start function
+    function onstart() {
+      sec++;
+      // if seconds = 60 count minutes
+      if (sec == 60) {
+        sec = 0;
+        mins++;
+        // if minutes = 60 count hour
+        if (sec == 60) {
+          mins = 0;
+          hrs++;
+        }
+      }
+
+      upDateText(sec, mins, hrs);
     }
     // reset button
-    if (clickedbtn.contains("reset")) {
+    function reset() {
+      clearInterval(timeinterval);
+      sec = 0;
+      mins = 0;
+      hrs = 0;
+      upDateText(sec, mins, hrs);
+    }
+    // stop function
+    function stop() {
+      clearInterval(timeinterval);
+    }
+    // 00:00:00 function
+    function twoDigit(value) {
+      return value < 10 ? "0" + value : value;
+    }
+
+    let clickedBtn = e.currentTarget.classList;
+    //  start button
+    if (clickedBtn.contains("start")) {
+      timeinterval = setInterval(onstart, 1000);
+    }
+    // reset button
+    if (clickedBtn.contains("reset")) {
       reset();
     }
     // stop button
-    if (clickedbtn.contains("stop")) {
+    if (clickedBtn.contains("stop")) {
       stop();
     }
   });
-  // startfunction
-  function onstart() {
-    startmilli++;
-    // if millisecond = 60 count seconds as 1
-    if (startmilli == 60) {
-      startmilli = 0;
-      sec++;
-      // if seconds = 60 count minutes as 1
-      if (sec == 60) {
-        sec = 0;
-        min++;
-        // if minutes = 60 start from 0
-        if (min == 60) {
-          min = 0;
-        }
-      }
-    }
-    millisecons.textContent = twodigit(startmilli);
-    seconds.textContent = twodigit(sec);
-    minutes.textContent = twodigit(min);
-  }
-  // reset button
-  function reset() {
-    clearInterval(timeinterval);
-    startmilli = 0;
-    sec = 0;
-    min = 0;
-    millisecons.textContent = twodigit(startmilli);
-    seconds.textContent = twodigit(sec);
-    minutes.textContent = twodigit(min);
-  }
-  // stop function
-  function stop() {
-    clearInterval(timeinterval);
-  }
-  // 00:00:00 function
-  function twodigit(value) {
-    return value < 10 ? "0" + value : value;
-  }
 });
